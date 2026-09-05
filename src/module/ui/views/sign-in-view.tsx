dd7eded98d7d1d87ce137303ff7df4d0c3b8ecd4
Arpan Das <arpan62825@gmail.com>
2026-09-05 21:44:21 +0530

    chore: add complete logic for signup and signin

--- src/module/ui/views/sign-in-view.tsx
+++ src/module/ui/views/sign-in-view.tsx
@@ -1,74 +1,84 @@
 "use client";
 
-import { useActionState } from "react";
-
-import { OctagonAlert } from "lucide-react";
-
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Field, FieldLabel, FieldError } from "@/components/ui/field";
-import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
-import { signIn, type SignInState } from "@/module/ui/actions/auth";
 import Image from "next/image";
 
-const initialState: SignInState = {};
-
 export const SignInView = () => {
-  const [state, formAction, isPending] = useActionState(signIn, initialState);
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSignin = async (formData: FormData) => {
+    setIsPending(true);
+    const { data, error } = await authClient.signIn.email({
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+    });
+
+    if (error) {
+      console.log(error);
+      setIsPending(false);
+      return;
+    }
+
+    console.log(data);
+
+    setIsPending(false);
+  };
 
   return (
     <div className="flex flex-col gap-6 max-h-screen">
       <Card className="bg-muted overflow-hidden p-0 max-h-1/3">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form action={formAction} className="flex flex-col gap-6 p-6 md:p-8">
-            <div className="flex flex-col items-center text-center">
-              <h1 className="text-2xl font-bold">Welcome back</h1>
-              <p className="text-balance text-muted-foreground">
-                Sign in to your account
+          <div className="flex flex-col gap-6 ">
+            <form
+              action={handleSignin}
+              className="flex flex-col gap-6 p-6 md:p-8"
+            >
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <p className="text-balance text-muted-foreground">
+                  Sign in to your account
+                </p>
+              </div>
+
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="you@example.com"
+                />
+              </Field>
+
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="••••••••"
+                />
+              </Field>
+
+              <Button type="submit" className="w-full" disabled={isPending}>
+                {isPending ? "Signing in..." : "Sign In"}
+              </Button>
+            </form>
+            <div className="mb-6">
+              <p className="text-center text-muted-foreground">
+                Don&apos;t have an account?{" "}
+                <a href="/auth/sign-up" className="underline">
+                  Sign Up
+                </a>
               </p>
             </div>
-            {state?.message && (
-              <Alert variant="destructive">
-                <OctagonAlert className="size-4" />
-                <AlertTitle>{state.message}</AlertTitle>
-              </Alert>
-            )}
-
-            <Field>
-              <FieldLabel htmlFor="email">Email</FieldLabel>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="you@example.com"
-                defaultValue={state?.inputs?.email}
-                aria-invalid={!!state?.errors?.email}
-              />
-              {state?.errors?.email && (
-                <FieldError>{state.errors.email[0]}</FieldError>
-              )}
-            </Field>
-
-            <Field>
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="••••••••"
-                aria-invalid={!!state?.errors?.password}
-              />
-              {state?.errors?.password && (
-                <FieldError>{state.errors.password[0]}</FieldError>
-              )}
-            </Field>
-
-            <Button type="submit" className="w-full" disabled={isPending}>
-              {isPending ? "Signing in..." : "Sign In"}
-            </Button>
-          </form>
+          </div>
 
           <div className="hidden md:flex flex-col items-center justify-center w-full h-full bg-muted">
             <Image
--- src/module/ui/views/sign-up-view.tsx
+++ src/module/ui/views/sign-up-view.tsx
@@ -2,24 +2,18 @@
 
 import { useState } from "react";
 
-import { OctagonAlert } from "lucide-react";
-
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Field, FieldLabel, FieldError } from "@/components/ui/field";
-import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Field, FieldLabel } from "@/components/ui/field";
 
-import { signIn, type SignInState } from "@/module/ui/actions/auth";
 import Image from "next/image";
 import { authClient } from "@/lib/auth-client";
-
-const initialState: SignInState = {};
 
 export const SignUpView = () => {
   const [isPending, setIsPending] = useState(false);
 
-  const handelSubmit = async (formData: FormData) => {
+  const handleSignup = async (formData: FormData) => {
     setIsPending(true);
 
     const { data, error } = await authClient.signUp.email({
@@ -29,6 +23,7 @@ export const SignUpView = () => {
     });
 
     if (error) {
+      setIsPending(false);
       console.log(error);
       return;
     }
@@ -41,67 +36,61 @@ export const SignUpView = () => {
     <div className="flex flex-col gap-6 max-h-screen">
       <Card className="bg-muted overflow-hidden p-0 max-h-1/3">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form
-            action={handelSubmit}
-            className="flex flex-col gap-6 p-6 md:p-8"
-          >
-            <div className="flex flex-col items-center text-center">
-              <h1 className="text-2xl font-bold">Welcome</h1>
-              <p className="text-balance text-muted-foreground">
-                Create a new account
+          <div>
+            <form
+              action={handleSignup}
+              className="flex flex-col gap-6 p-6 md:p-8"
+            >
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome</h1>
+                <p className="text-balance text-muted-foreground">
+                  Create a new account
+                </p>
+              </div>
+
+              <Field>
+                <FieldLabel htmlFor="name">User Name</FieldLabel>
+                <Input
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="User Name"
+                />
+              </Field>
+
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="you@example.com"
+                />
+              </Field>
+
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="••••••••"
+                />
+              </Field>
+
+              <Button type="submit" className="w-full" disabled={isPending}>
+                {isPending ? "Signing up..." : "Sign Up"}
+              </Button>
+            </form>
+            <div className="mb-6">
+              <p className="text-center text-muted-foreground">
+                Already have an account?{" "}
+                <a href="/auth/sign-in" className="underline">
+                  Sign In
+                </a>
               </p>
             </div>
-            {/* {state?.message && (
-              <Alert variant="destructive">
-                <OctagonAlert className="size-4" />
-                <AlertTitle>{state.message}</AlertTitle>
-              </Alert>
-            )} */}
-
-            <Field>
-              <FieldLabel htmlFor="name">User Name</FieldLabel>
-              <Input
-                id="name"
-                name="name"
-                type="text"
-                placeholder="User Name"
-                // defaultValue={state?.inputs?.userName}
-              />
-            </Field>
-
-            <Field>
-              <FieldLabel htmlFor="email">Email</FieldLabel>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="you@example.com"
-                // defaultValue={state?.inputs?.email}
-                // aria-invalid={!!state?.errors?.email}
-              />
-              {/* {state?.errors?.email && (
-                <FieldError>{state.errors.email[0]}</FieldError>
-              )} */}
-            </Field>
-
-            <Field>
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="••••••••"
-                // aria-invalid={!!state?.errors?.password}
-              />
-              {/* {state?.errors?.password && (
-                <FieldError>{state.errors.password[0]}</FieldError>
-              )} */}
-            </Field>
-
-            <Button type="submit" className="w-full" disabled={isPending}>
-              {isPending ? "Signing up..." : "Sign Up"}
-            </Button>
-          </form>
+          </div>
 
           <div className="hidden md:flex flex-col items-center justify-center w-full h-full bg-muted">
             <Image
